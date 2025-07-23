080c784e3cb35eedda016ed95c40544e3e3f4a83
Fixron Maulana <fikronmaulana27@gmail.com>
2025-07-23 09:38:23 +0700

    menambahkan routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Transactions::index'); // Mengatur halaman utama ke daftar transaksi
+$routes->get('/transactions', 'Transactions::index');
+$routes->get('/transactions/create', 'Transactions::create');
+$routes->post('/transactions/store', 'Transactions::store');
